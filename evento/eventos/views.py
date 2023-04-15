944fd180d55fef57d9fc313acbf9f88a75838fe5
AndreCabrales <acabralmorais@gmail.com>
2023-04-15 17:32:01 -0300

    app cliente

--- evento/eventos/views.py
+++ evento/eventos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Evento
+from .models import Certificado, Evento
 from django.urls import reverse
 from django.contrib import messages
 from django.contrib.messages import constants
@@ -10,6 +10,10 @@ import csv
 from secrets import token_urlsafe
 import os
 from django.conf import settings
+from io import BytesIO  
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from PIL import Image, ImageDraw, ImageFont
+import sys  
 
 
 @login_required
@@ -89,7 +93,7 @@ def inscrever_evento(request, id):
         return redirect(f'/eventos/inscrever_evento/{id}')
         # return redirect(reverse('inscrever_evento', kwargs={'id': id}))
 
-
+@login_required 
 def participantes_evento(request, id):
     evento = get_object_or_404(Evento, id=id)
 
@@ -99,8 +103,8 @@ def participantes_evento(request, id):
     if request.method == 'GET':
         participantes = evento.participantes.all()[::3] # split - filtra parte da lista de 1 até 3
         return render(request, 'participantes_evento.html', {'participantes': participantes, 'evento': evento})
-    
 
+@login_required 
 def gerar_csv(request, id):
     evento = get_object_or_404(Evento, id=id)
 
@@ -119,3 +123,78 @@ def gerar_csv(request, id):
             writer.writerow(x)
 
     return redirect(f'/media/{token}')
+
+@login_required 
+def certificados_evento(request, id):
+    evento = get_object_or_404(Evento, id=id)
+    if not evento.criador == request.user:
+        raise Http404('Esse evento não é seu')
+    if request.method == "GET":
+        qtd_certificados = evento.participantes.all().count() - Certificado.objects.filter(evento=evento).count()
+        return render(request, 'certificados_evento.html', {'evento': evento, 'qtd_certificados': qtd_certificados})
+
+@login_required 
+def gerar_certificado(request, id):
+
+    evento = get_object_or_404(Evento, id=id)
+    if not evento.criador == request.user:
+        raise Http404('Esse evento não é seu')
+
+    path_template = os.path.join(settings.BASE_DIR, 'templates/static/evento/img/template_certificado.png')
+    path_fonte = os.path.join(settings.BASE_DIR, 'templates/static/fontes/arimo.ttf')
+    
+    for participante in evento.participantes.all():
+        # TODO: Validar se já existe certificado desse participante para esse evento
+        #abre a imagem
+        img = Image.open(path_template)
+        path_template = os.path.join(settings.BASE_DIR, 'templates/static/evento/img/template_certificado.png')
+        # escreve
+        draw = ImageDraw.Draw(img)
+
+        fonte_nome = ImageFont.truetype(path_fonte, 60)
+        fonte_info = ImageFont.truetype(path_fonte, 30)
+
+        # escrevendo na img. (posição), (o que vou escrever ""), fonte, fill é a cor,
+        draw.text((230, 651), f"{participante.username}", font=fonte_nome, fill=(0, 0, 0))
+        draw.text((761, 782), f"{evento.nome}", font=fonte_info, fill=(0, 0, 0))
+        draw.text((816, 849), f"{evento.carga_horaria} horas", font=fonte_info, fill=(0, 0, 0))
+
+        # simular para salvar numa variável
+        output = BytesIO()
+        # salva na variável output
+        img.save(output, format="PNG", quality=100)
+        # volta o cursos para o início do doc
+        output.seek(0)
+        #classe de upload q o Django entende
+        img_final = InMemoryUploadedFile(output,
+                                        'ImageField',
+                                        f'{token_urlsafe(8)}.png',
+                                        'image/jpeg',
+                                        sys.getsizeof(output),
+                                        None)
+        certificado_gerado = Certificado(
+            certificado=img_final,
+            participante=participante,
+            evento=evento,
+        )
+        certificado_gerado.save()
+    
+    messages.add_message(request, constants.SUCCESS, 'Certificados gerados')
+    # kwargs, volta para a página passando o id para do eventos q estava
+    return redirect(reverse('certificados_evento', kwargs={'id': evento.id}))
+
+@login_required 
+def procurar_certificado(request, id):
+
+    evento = get_object_or_404(Evento, id=id)
+    if not evento.criador == request.user:
+        raise Http404('Esse evento não é seu')
+    
+    email = request.POST.get('email')
+
+    certificado = Certificado.objects.filter(evento=evento).filter(participante__email=email).first()
+    if not certificado:
+        messages.add_message(request, constants.WARNING, 'Certificado não encontrado')
+        return redirect(reverse('certificados_evento', kwargs={'id': evento.id}))
+    
+    return redirect(certificado.certificado.url)
